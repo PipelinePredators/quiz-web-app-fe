6a87e285fe754c2f85144c1af24e5650aabb9d00
enochviewu95 <viewuenoch@gmail.com>
2022-09-30 04:30:01 +0000

    Made changes to api and fixed bugs

--- src/Redux/StudentSlice.js
+++ src/Redux/StudentSlice.js
@@ -5,11 +5,15 @@ import storage from "redux-persist/lib/storage";
 export const StudentSlice = createSlice({
     name: 'student',
     initialState: {
-        token: ''
+        token: '',
+        studentDetails: ''
     },
     reducers: {
         setStudentState: (state, action) => {
             state.token = action.payload;
+        },
+        setStudentDetailsState: (state, action) => {
+            state.studentDetails = action.payload;
         },
         removeStudentState: () => {
             storage.removeItem('persist:student')
@@ -21,7 +25,9 @@ export const StudentSlice = createSlice({
 /* Exporting the action creator function. */
 export const { setStudentState } = StudentSlice.actions;
 
-export const {removeStudentState} = StudentSlice.actions;
+export const { setStudentDetailsState } = StudentSlice.actions;
+
+export const { removeStudentState } = StudentSlice.actions;
 
 /* Exporting the reducer function. */
 export default StudentSlice.reducer
